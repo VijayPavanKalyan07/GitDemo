4405f5f82b450e2167f8d625263150011d35353b
pavankalyan <vijaypavankalyanreddy@gmail.com>
2026-07-28 19:21:59 +0530

    Added car class

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Car
+{
+    private int price;
+    private int engineSize;
+
+    public Car(int price, int engineSize) {
+        this.price = price;
+        this.engineSize = engineSize;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getEngineSize() {
+        return engineSize;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setEngineSize(int engineSize) {
+        this.engineSize = engineSize;
+    }
+}
